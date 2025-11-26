54df8114468641a2845dcfc68715c80dbcec73f8
F99 <farajiomar99@gmail.com>
2025-11-26 21:09:00 +0100

    feat: add method to find non-deleted products in ProductRepository

--- src/main/java/com/smartshop/repository/ProductRepository.java
+++ src/main/java/com/smartshop/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import com.smartshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    // find non-deleted products
+    List<Product> findByDeletedFalse();
 }
