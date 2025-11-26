f302a5c484c1de2e9d651b8d3afe35678be58e71
F99 <farajiomar99@gmail.com>
2025-11-26 11:56:23 +0100

    add repository interfaces

---
+++ src/main/java/com/smartshop/repository/PayementRepository.java
@@ -0,0 +1,9 @@
+package com.smartshop.repository;
+
+import com.smartshop.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PayementRepository extends JpaRepository<Payment, Long> {
+}
