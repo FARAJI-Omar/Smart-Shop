3afffebe5f071de1dde22fc0d58f95314f312d71
F99 <farajiomar99@gmail.com>
2025-11-28 16:17:09 +0100

    feat: add PromoCodeService interface and existsByCode method in PromoCodeRepository

--- src/main/java/com/smartshop/repository/PromoCodeRepository.java
+++ src/main/java/com/smartshop/repository/PromoCodeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PromoCodeRepository extends JpaRepository<PromoCode,Long> {
+    boolean existsByCode(String code);
 }
