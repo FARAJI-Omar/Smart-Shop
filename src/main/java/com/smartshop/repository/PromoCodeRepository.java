bdc3ad419c3ea8be1ad83c51f87aa30749578b8a
F99 <farajiomar99@gmail.com>
2025-11-28 16:27:12 +0100

    feat: implement promo code discount application in order creation

--- src/main/java/com/smartshop/repository/PromoCodeRepository.java
+++ src/main/java/com/smartshop/repository/PromoCodeRepository.java
@@ -4,7 +4,10 @@ import com.smartshop.entity.PromoCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PromoCodeRepository extends JpaRepository<PromoCode,Long> {
     boolean existsByCode(String code);
+    Optional<PromoCode> findByCode(String code);
 }
