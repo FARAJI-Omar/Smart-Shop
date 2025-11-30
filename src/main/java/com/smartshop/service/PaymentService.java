eb748f8304aab47f8b420c0993e9b2188929e08a
F99 <farajiomar99@gmail.com>
2025-11-30 15:58:43 +0100

    feat: add PaymentService interface with methods for payment management

---
+++ src/main/java/com/smartshop/service/PaymentService.java
@@ -0,0 +1,16 @@
+package com.smartshop.service;
+
+import com.smartshop.dto.request.PaymentCreateDTO;
+import com.smartshop.dto.request.PaymentUpdateStatusDTO;
+import com.smartshop.dto.response.PaymentResponseDTO;
+
+import java.util.List;
+
+public interface PaymentService {
+    PaymentResponseDTO addPayment(PaymentCreateDTO dto);
+    PaymentResponseDTO updatePaymentStatus(Long paymentId, PaymentUpdateStatusDTO dto);
+    PaymentResponseDTO getPaymentById(Long id);
+    List<PaymentResponseDTO> getPaymentsByOrderId(Long orderId);
+    List<PaymentResponseDTO> getAllPayments();
+}
+
